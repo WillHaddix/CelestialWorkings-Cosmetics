ac943c3fef8fe48ba4943a7f5819306fdb938159
WillHaddix <WillHaddix@users.noreply.github.com>
2016-08-03 10:07:04 -0700

    common folder

---
+++ common/defines/celest_cosmetics_defines.lua
@@ -0,0 +1,45 @@
+-------------------------------------------------------------------
+
+-- Some of this is written by Haddix
+
+-- But, most is..... :smile:
+
+----------------------------
+
+-- Start of Cosmetics Defines
+
+
+-- Sectors
+
+NDefines.NGraphics.MAPNAME_SECTOR_SCALE				        					= 6;
+NDefines.NGraphics.MAPNAME_SECTOR_OFFSET			        					= 10;
+
+-- For the rebuilt ethics and gov mod
+
+NDefines.NGameplay.ETHOS_MAX_POINTS												= 8;
+
+-- Galaxy Size
+NDefines.NCamera.SYSTEM_SPACE_SCALE_MULT 										= 40.0;
+NDefines.NCamera.GALAXY_SPACE_SCALE_MULT 										= 2.0;
+
+-- Happyness 
+NDefines.NPop.HAPPINESS_PROGRESS_BAR_GREEN 										= 75;
+NDefines.NPop.HAPPINESS_PROGRESS_BAR_YELLOW 									= 35;
+
+-- NGraphics
+NDefines.NGraphics.MISSED_BEAM_LENGTH 											= 500.0;
+NDefines.NGraphics.SHIP_RANDOM_HEIGHT_OFFSET 									= 40.0;
+NDefines.NGraphics.MISSILE_HEIGHT_OFFSET 										= 27.5;
+NDefines.NGraphics.MISSILE_RANDOM_OFFSET_MIN_RADIUS 							= 0;
+NDefines.NGraphics.MISSILE_RANDOM_OFFSET_MAX_RADIUS 							= 0;
+NDefines.NGraphics.TRAILS_MISSILE_ALPHA_FADE 									= 3;
+NDefines.NGraphics.TRAILS_MISSILE_BASE_WIDTH 									= 2.0;
+NDefines.NGraphics.BALLISTIC_PROJECTILE_MISSED_LIFETIME 						= 5.0;
+NDefines.NGraphics.DEFAULT_PLANET_PLANE											= -175.0;
+
+-- No idea
+NDefines.NGraphics.DEFAULT_PLANET_PLANE 										= -200.0;
+
+-- This is the END! 
+
+-- Cheers!
